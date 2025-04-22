029cc46f0278c0524e311806830512f117c775ae
Harsha Nimeda Sirithunga <harshanimeda@gmail.com>
2025-04-22 20:06:09 +0530

    update AdminController

--- BackEnd/src/main/java/org/example/backend/controller/AdminController.java
+++ BackEnd/src/main/java/org/example/backend/controller/AdminController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("api/v1/admin")
 public class AdminController {
+
     @GetMapping("/test1")
     @PreAuthorize("hasAuthority('ADMIN')")
     public String check(){
