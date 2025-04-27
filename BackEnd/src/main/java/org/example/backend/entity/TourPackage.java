84aecd4fd03eb54819583a13da0f8eefc28917b0
Harsha Nimeda Sirithunga <harshanimeda@gmail.com>
2025-04-27 11:20:02 +0530

    update admin dashboard

--- BackEnd/src/main/java/org/example/backend/entity/TourPackage.java
+++ BackEnd/src/main/java/org/example/backend/entity/TourPackage.java
@@ -10,7 +10,6 @@ public class TourPackage {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
     private String packageName;
     private String description;
     private Double price;
